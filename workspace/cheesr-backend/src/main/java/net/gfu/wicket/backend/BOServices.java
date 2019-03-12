147306cc74302e3692e95fde1a258460eea3a32a
Jan Luehr <jan.luehr@anderscore.com>
2019-03-12 15:53:37 +0100

    Optimistic Locking

--- workspace/cheesr-backend/src/main/java/net/gfu/wicket/backend/BOServices.java
+++ workspace/cheesr-backend/src/main/java/net/gfu/wicket/backend/BOServices.java
@@ -1,8 +1,6 @@
 package net.gfu.wicket.backend;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import javax.annotation.ManagedBean;
 
@@ -11,41 +9,68 @@ import net.gfu.wicket.backend.bo.Cheese;
 
 @ManagedBean
 public class BOServices {
-	
-	private final List<Cheese> cheeses = Arrays.asList(
-			new Cheese("Gouda", "Gouda is a yellowish Dutch[...]",
-					 1.65),
-					new Cheese("Edam", "Edam (Dutch Edammer) is a D[...]",
-					 1.05),
-					new Cheese("Maasdam", "Maasdam cheese is a Dutc[...]",
-					 2.35),
-					new Cheese("Brie", "Brie is a soft cows' milk c[...]",
-					 3.15),
-					new Cheese("Buxton Blue", "Buxton Blue cheese i[...]",
-					 0.99),
-					new Cheese("Parmesan", "Parmesan is a grana, a [...]",
-					 1.99),
-					new Cheese("Cheddar", "Cheddar cheese is a hard[...]",
-					 2.95),
-					new Cheese("Roquefort", "Roquefort is a ewe's-m[...]",
-					 1.67),
-					new Cheese("Boursin", "Boursin Cheese is a soft[...]",
-					 1.33),
-					new Cheese("Camembert", "Camembert is a soft, c[...]",
-					 1.69),
-					new Cheese("Emmental", "Emmental is a yellow, m[...]",
-					 2.39),
-					new Cheese("Reblochon", "Reblochon is a French [...]",
-					 2.99));
+
+	private static final HashMap<Integer, Cheese> cheeses = new HashMap<>();
+
+	static {
+		cheeses.put(1, new Cheese(1, "Gouda", "Gouda is a yellowish Dutch[...]", 1.65));
+
+		cheeses.put(2, new Cheese(2, "Edam", "Edam (Dutch Edammer) is a D[...]",
+				1.05));
+		cheeses.put(3, new Cheese(3, "Maasdam", "Maasdam cheese is a Dutc[...]",
+				2.35));
+		cheeses.put(4, new Cheese(4, "Brie", "Brie is a soft cows' milk c[...]",
+				3.15));
+		cheeses.put(5, new Cheese(5, "Buxton Blue", "Buxton Blue cheese i[...]",
+				0.99));
+		cheeses.put(6, new Cheese(6, "Parmesan", "Parmesan is a grana, a [...]",
+				1.99));
+		cheeses.put(7, new Cheese(7, "Cheddar", "Cheddar cheese is a hard[...]",
+				2.95));
+		cheeses.put(8, new Cheese(8, "Roquefort", "Roquefort is a ewe's-m[...]",
+				1.67));
+		cheeses.put(9, new Cheese(9, "Boursin", "Boursin Cheese is a soft[...]",
+				1.33));
+		cheeses.put(10, new Cheese(10, "Camembert", "Camembert is a soft, c[...]",
+				1.69));
+		cheeses.put(11, new Cheese(11, "Emmental", "Emmental is a yellow, m[...]",
+				2.39));
+		cheeses.put(12, new Cheese(12, "Reblochon", "Reblochon is a French [...]",
+				2.99));
+	}
+
+	public BOServices(){
+
+	}
+
 	
 	/**
 	 * Ermittet alle verfuegbaren Kaesesorten
 	 * @return - Liste aller Sorten
 	 */
 	public List<Cheese> allCheeses(){
-		return Collections.unmodifiableList(cheeses);
+		return Collections.unmodifiableList(new ArrayList<>(cheeses.values()));
 	}
-	
+
+	public void updateCheese(Cheese cheese, int baseLockingVersion) {
+		Cheese c = load(cheese.getId());
+		if(cheese.getLockingVersion() != baseLockingVersion) {
+			throw new IllegalStateException(
+					String.format("Trying to update old version. Base version is %d, current version is %d",
+							baseLockingVersion,cheese.getLockingVersion()));
+		}
+		c.setName(cheese.getName());
+		c.setDescription(cheese.getDescription());
+		c.setPrice(cheese.getPrice());
+		c.incrementLock();
+
+	}
+
+	public Cheese load(int id) {
+		return cheeses.get(id);
+	}
+
+
 	/**
 	 * Uebergibt eine Bestellung zur Abwicklung ins Backend-System
 	 */
--- workspace/cheesr-backend/src/main/java/net/gfu/wicket/backend/bo/Cheese.java
+++ workspace/cheesr-backend/src/main/java/net/gfu/wicket/backend/bo/Cheese.java
@@ -1,21 +1,26 @@
 package net.gfu.wicket.backend.bo;
 
 import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
 
-public class Cheese implements Serializable{
+public class Cheese extends Lockable implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private String name;
 	private String description;
 	private double price;
-	
+	private int id;
+
 	public Cheese(){
 	}
 	
-	public Cheese(String name, String description, double price) {
+	public Cheese(int id, String name, String description, double price) {
 		this.name = name;
 		this.description = description;
 		this.price = price;
+		this.id = id;
+		this.setLockingVersion(1);
 	}
 
 	public String getName() {
@@ -37,44 +42,31 @@ public class Cheese implements Serializable{
 		this.price = price;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		long temp;
-		temp = Double.doubleToLongBits(price);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		return result;
+	public int getId() {
+		return id;
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cheese other = (Cheese) obj;
-		if (description == null) {
-			if (other.description != null)
-				return false;
-		} else if (!description.equals(other.description))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
-			return false;
-		return true;
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Cheese cheese = (Cheese) o;
+		return id == cheese.id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
 	}
 
 	@Override
 	public String toString() {
-		return "Cheese [name=" + name + ", description=" + description + ", price=" + price + "]";
+		return "Cheese{" +
+				"name='" + name + '\'' +
+				", description='" + description + '\'' +
+				", price=" + price +
+				", lockingVersion=" + getLockingVersion() +
+				", id=" + id +
+				'}';
 	}
 }
