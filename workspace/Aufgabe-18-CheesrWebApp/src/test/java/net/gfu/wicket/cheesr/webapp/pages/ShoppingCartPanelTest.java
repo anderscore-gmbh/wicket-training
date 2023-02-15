0485dbcaf354e463353a442b4e45034f281fd6b3
Patrick Möbius <patrick.moebius@anderscore.com>
2023-02-15 11:30:23 +0100

    Junit version 4, entsprechende Imports angepasst.

--- workspace/Aufgabe-18-CheesrWebApp/src/test/java/net/gfu/wicket/cheesr/webapp/pages/ShoppingCartPanelTest.java
+++ workspace/Aufgabe-18-CheesrWebApp/src/test/java/net/gfu/wicket/cheesr/webapp/pages/ShoppingCartPanelTest.java
@@ -10,21 +10,22 @@ import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.util.tester.TagTester;
 import org.apache.wicket.util.tester.WicketTester;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ShoppingCartPanelTest {
 
 	private WicketTester tester;
 	private Cart cart;
 
-	@BeforeEach
+	@Before
+//	@BeforeEach
 	public void setUp() {
 		tester = new WicketTester(new TestWicketApplication());
 
